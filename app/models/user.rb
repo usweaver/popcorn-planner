736b7548587e3a22f4802667486322fbc8b04156
Clément THOREZ <clement.thorez1@gmail.com>
2024-11-26 17:07:06 +0100

    finish graine

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :events
+  has_many :members
   has_many :groups, through: :members
   has_one_attached :profile_picture
 end
