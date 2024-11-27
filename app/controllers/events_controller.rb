b882e6d4b3349c61e857af2c72f22b150e753b61
Florian CHAPUIS <fchapuis20@gmail.com>
2024-11-27 15:01:46 +0100

    Index events done

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,10 @@
 class EventsController < ApplicationController
   def index
+    all_invitations = Event.where(group_id: current_user.groups.ids).where.not(user_id: current_user.id)
+    @pending_events = all_invitations.joins(movie_events: :votes).where('date >= ?', Date.today)
+    @done_events = all_invitations.joins(movie_events: :votes).where('date < ?', Date.today)
+    # @pending_events = []
+    # @done_events = []
   end
 
   def show
