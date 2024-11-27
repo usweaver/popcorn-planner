798e6635d0b4ee605ee98d9cd39fe162c38d1b86
Florian CHAPUIS <fchapuis20@gmail.com>
2024-11-27 16:34:36 +0100

    Index movies done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,13 @@
 class MoviesController < ApplicationController
   def index
+    # Récupérer les IDs des groupes auxquels appartient l'utilisateur
+    user_group_ids = current_user.groups.ids
+
+    # Récupérer les films répondant aux critères
+    @movies = Movie.joins(movie_events: :event)
+                       .where(movie_events: { selected_movie: true })
+                       .where('events.user_id = ? OR events.group_id IN (?)', current_user.id, user_group_ids)
+                       .where('events.date < ?', Date.today)
   end
 
   def show
