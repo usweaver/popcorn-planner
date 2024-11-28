cfefcc2abc761af5e0b2a1fccfcb1e6da4e0e779
Florian CHAPUIS <fchapuis20@gmail.com>
2024-11-28 13:28:01 +0100

    First tweaks

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,11 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
-    all_invitations = Event.where(group_id: current_user.groups.ids).where.not(user_id: current_user.id)
-    @pending_events = all_invitations.joins(movie_events: :votes).where('date >= ?', Date.today)
-    @invitations = all_invitations.where.not(id: @pending_events.ids)
+    if user_signed_in?
+      all_invitations = Event.where(group_id: current_user.groups.ids).where.not(user_id: current_user.id)
+      @pending_events = all_invitations.joins(movie_events: :votes).where('date >= ?', Date.today)
+      @invitations = all_invitations.where.not(id: @pending_events.ids)
+    end
     # @pending_events = []
     # @invitations = []
   end
---
+++ app/javascript/controllers/avatar_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "@hotwired/stimulus";
+
+// Connects to data-controller="avatar"
+export default class extends Controller {
+  static targets = ["menu"];
+
+  connect() {
+    console.log("Hello from avatar controller");
+  }
+
+  toggle() {
+    if (this.menuTarget.classList.contains("opacity-0")) {
+      this.menuTarget.classList.remove("opacity-0");
+      this.menuTarget.classList.add("opacity-100");
+    } else {
+      this.menuTarget.classList.remove("opacity-100");
+      this.menuTarget.classList.add("opacity-0");
+    }
+  }
+}
