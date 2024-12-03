b09f4109186952386b1b28a71e8a3ce3ccce1d24
Clément THOREZ <clement.thorez1@gmail.com>
2024-12-03 17:18:33 +0100

    fix all

--- app/models/movie_event.rb
+++ app/models/movie_event.rb
@@ -2,7 +2,7 @@ class MovieEvent < ApplicationRecord
   belongs_to :event
   belongs_to :movie, optional: true
   has_many :movie_comments
-  has_one :vote
+  has_one :vote, dependent: :destroy
 
   has_one :user, through: :vote
 end
